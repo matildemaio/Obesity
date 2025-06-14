47b646738ecc1665a8fd747df414cc2e12eae9b1
demirgunbahar <d.gunbahar@student.vu.nl>
2025-06-14 16:08:59 +0200

    Update data_combining.R

--- data_combining.R
+++ data_combining.R
@@ -18,3 +18,17 @@ combined$year <- combined$year %>% replace(combined$year == 3, 2022)
 
 col_numbers <- c(2, seq(3,604, by = 12))
 combined<-combined[,col_numbers]
+
+library(dplyr)
+library(tidyr)
+income2019 <- income2019 %>% rename(income = X2019)
+income2021 <- income2021 %>% rename(income = X2021)
+income2022 <- income2022 %>% rename(income = X2022)
+income2019 <- income2019 %>% mutate(year = 2019)
+income2021 <- income2021 %>% mutate(year = 2021)
+income2022 <- income2022 %>% mutate(year = 2022)
+combined_income <- bind_rows(income2019, income2021, income2022)
+average_income <- combined_income %>%
+  group_by(GeoName) %>%
+  summarise(avg_income = mean(income, na.rm = TRUE)) %>%
+  arrange(desc(avg_income))  # optional: sort by highest income
